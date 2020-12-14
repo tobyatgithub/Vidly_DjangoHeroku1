2d00d2d42031d2c8de0803c0fbeced35ff5785a9
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-14 00:16:21 -0500

    register the changes in models.py by updaing settings. and call migrate.

--- movies/models.py
+++ movies/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Genre(models.Model):
-    name = models.CharField(max_len=255)
+    name = models.CharField(max_length=255)
 
 class Movie(models.Model):
-    title = models.CharField(max_len=255)
+    title = models.CharField(max_length=255)
     release_year = models.IntegerField()
     number_in_stock = models.IntegerField()
-    daily_rate = model.FloatField()
-    genre = model.ForeignKey(Genre, on_delete=model.CASCADING)
+    daily_rate = models.FloatField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     # cacading = if a genre is deleted, than all movies with same genre will be deleted.
