aa75eee4fb9ed0dae3a933330ce6f8172af2cef9
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-14 01:07:03 -0500

    connecting database and view functions.

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+
+    return HttpResponse(output)
