fb8c1bba02701bd88c1c2fd2165ba6814b01be80
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-14 00:50:24 -0500

    create admin, and customize admin display fields.

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Movie, Genre
 # Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'number_in_stock', 'daily_rate')
+    # to hide the date_created field from the admin add movie page
+    exclude = ('date_created',) 
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
+
