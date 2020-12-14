b230cfe878b486f5f97f2831e96706cf845239d4
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-14 01:20:39 -0500

    render the page to make it looks nicer.

--- movies/views.py
+++ movies/views.py
@@ -2,9 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Movie
 
-# Create your views here.
+# if see pylint complain:  pip install pylint-django 
 def index(request):
     movies = Movie.objects.all()
-    output = ', '.join([m.title for m in movies])
-
-    return HttpResponse(output)
+    # we pass data throught context arg
+    return render(request, 'index.html', context={'movies': movies}) 
