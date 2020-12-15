77e1f64de08fb58d70f848b6c2c4068f417f8261
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-15 01:35:46 -0800

    adding 404 response via try-except.

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from .models import Movie
 
@@ -9,6 +9,9 @@ def index(request):
     return render(request, 'movies/index.html', context={'movies': movies}) 
 
 def detail(request, movie_id):
-    movie = Movie.objects.get(id=movie_id)
-    
-    return render(request, 'movies/detail.html', context={'movie': movie})
+    try:
+        movie = Movie.objects.get(id=movie_id)
+        return render(request, 'movies/detail.html', context={'movie': movie})
+    except Movie.DoesNotExist:
+        raise Http404()
+
