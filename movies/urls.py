41a956f7731b1d927b07adf3755dccb85abdf33b
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-15 01:44:11 -0800

    link movie title to detail page using app_name magic..

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'movies' # app_name is a reserved variable for django to recognize namespace pattern for url.
 # here we map url endpoints to view functions
 urlpatterns = [
-    path("", views.index, name="movies_index"), # notice we are not calling it here
-    path("<int:movie_id>", views.detail, name="movies_detail") # the "int:" limit the pattern to be int only
+    path("", views.index, name="index"), # notice we are not calling it here
+    path("<int:movie_id>", views.detail, name="detail") # the "int:" limit the pattern to be int only
 ]
