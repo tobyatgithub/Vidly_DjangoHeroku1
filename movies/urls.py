0d01bc0a25d688b84f760a2c33bf6afb5b380157
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-15 01:30:05 -0800

    adding detail page for each movie

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # here we map url endpoints to view functions
 urlpatterns = [
-    path("", views.index, name="index"), # notice we are not calling it here
+    path("", views.index, name="movies_index"), # notice we are not calling it here
+    path("<int:movie_id>", views.detail, name="movies_detail") # the "int:" limit the pattern to be int only
 ]
--- movies/views.py
+++ movies/views.py
@@ -7,3 +7,6 @@ def index(request):
     movies = Movie.objects.all()
     # we pass data throught context arg
     return render(request, 'movies/index.html', context={'movies': movies}) 
+
+def detail(request, movie_id):
+    return HttpResponse(movie_id)
